4a49d13457c7600e48ad73b24fd06ab995bd84ab
Koushik Yerraguntla <koushikyerraguntla@gmail.com>
2025-04-10 00:34:48 +0530

    Set-Up✔

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-4xl text-cyan-800"> hiii </h1>
+    <Button className="m-10">hi</Button>
+    </>
+  )
+}
+
+export default App
