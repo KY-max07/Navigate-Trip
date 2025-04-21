73fed530797b08a510627cc7c634a917d5623980
Koushik Yerraguntla <koushikyerraguntla@gmail.com>
2025-04-21 23:08:06 +0530

    minor changes made

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,30 +1,31 @@
 import Footer from "@/components/Common/Footer";
 import Header from "@/components/Common/Header";
-import ScrollToTop from "@/components/ScrolltoTop";
+import ScrollToTop from "@/components/ScrollToTop";
 import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import Lenis from 'lenis'
 
 const UserLayout = () => {
+  
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1,
-      lerp:0.5, // Transition speed (higher value makes it slower)
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function
-      smooth: true, // Smooth scroll enabled
+      lerp:0.5, 
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+      smooth: true, 
     });
 
-    // Function to handle Lenis' RAF (requestAnimationFrame) loop
+    
     const raf = (time) => {
-      lenis.raf(time);  // Render the animation frame
-      requestAnimationFrame(raf);  // Keep the animation loop going
+      lenis.raf(time); 
+      requestAnimationFrame(raf);  
     };
 
     // Start the animation loop
     requestAnimationFrame(raf);
 
-    // Cleanup function to destroy Lenis instance when the component unmounts
+    
     return () => {
       lenis.destroy();
     };
