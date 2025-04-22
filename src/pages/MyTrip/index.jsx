3bc92032f1afc3829bffad27602d5a36915f1a14
Koushik Yerraguntla <koushikyerraguntla@gmail.com>
2025-04-22 16:33:55 +0530

    minor changes made

--- src/pages/MyTrip/index.jsx
+++ src/pages/MyTrip/index.jsx
@@ -10,9 +10,16 @@ import MyTripCards from "./MyTripCards";
 const MyTrip = () => {
   const navigate = useNavigate;
   const [trips, setTrips] = useState([]);
+  const [show, setShow ] = useState(false);
 
   useEffect(() => {
+    const timer = setTimeout(() => {
+    setShow(true);
+    }, 2000); // 2 seconds
+
+   
     getTrips();
+    return () => clearTimeout(timer);
   },[]);
 
   const getTrips = async () => {
@@ -34,7 +41,7 @@ const MyTrip = () => {
       // doc.data() is never undefined for query doc snapshots
      
       setTrips((prev) => [...prev, doc.data()]);
-    },[]);
+    });
   };
   
   return (
@@ -45,7 +52,21 @@ const MyTrip = () => {
         <h2 className="text-3xl md:text-4xl text-side font-secondary mb-3 md:mb-5">
           My-Trips
         </h2>
-        {!trips ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 min-h-83">
+        {!show ? (
+          [1,2,3,4,5,6].map((i,index)=>(
+            <div key={index} className="h-60 md:h-90 bg-gray-300 animate-pulse rounded-xl">
+                
+            </div>
+          ))
+          
+        ) : (
+        <div>
+          {trips.length>0?trips.reverse().map((trip, index) => (
+            <div key={index}>
+              <MyTripCards trip={trip} />
+            </div>
+          )):
           <div className="min-h-120 md:min-h-83  flex flex-col gap-1 md:gap-6 items-center justify-center text-center">
             <h2 
             className="  text-red-700 font-secondary text-2xl md:text-3xl ">
@@ -53,21 +74,11 @@ const MyTrip = () => {
             </h2>
             
           </div>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 min-h-83">
-          {trips.length>0?trips.reverse().map((trip, index) => (
-            <div key={index}>
-              <MyTripCards trip={trip} />
-            </div>
-          )):
-          [1,2,3,4,5,6].map((i,index)=>(
-            <div key={index} className="h-60 md:h-90 bg-gray-300 animate-pulse rounded-xl">
-                
-            </div>
-          ))
+          
           }
         </div>
         )}
+        </div>
         
       </div>
       
